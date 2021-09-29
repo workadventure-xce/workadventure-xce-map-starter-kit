b2479c10fba8f07c6315396adac5b41af7e7fa99
Tim Shirley <tim.shirley@fnordkollektiv.de>
2021-09-29 17:22:56 +0200

    Allow cross origin request

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ module.exports = {
     devServer: {
         static: {
             directory: './'
+        },
+        headers: {
+            "Access-Control-Allow-Origin": "*",
         }
     }
 }
